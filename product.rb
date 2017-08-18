0cb68e7d3ed011f08eda34cdfaf06b6e7f8ded22
sbcreates <drawsarahb@gmail.com>
2017-08-18 13:30:43 -0400

    Need to work on removing item from shopping cart but all other methods work

--- product.rb
+++ product.rb
@@ -3,11 +3,35 @@ class Product
   def initialize(name, base_price, tax_rate)
     @name = name
     @base_price = base_price
-    @tax_rate = tax_rate
+    @tax_rate = tax_rate + 1
   end
 
-  def total
+  def product_total
     @total = base_price * (tax_rate + 1)
+  end
+
+  def product_name
+    @name
+  end
+
+  def product_name=(new_name)
+    @name = new_name
+  end
+
+  def product_base_price
+    @base_price
+  end
+
+  def product_base_price=(new_price)
+    @base_price = new_price
+  end
+
+  def product_tax_rate
+    @tax_rate
+  end
+
+  def product_tax_rate=(new_tax_rate)
+    @tax_rate = new_tax_rate
   end
 
 end
--- shopping_cart.rb
+++ shopping_cart.rb
@@ -1,7 +1,60 @@
 require_relative('product.rb')
-
-  @@in_cart = []
+require 'pry'
 
 class Cart
 
+  @@items_customers_purchased = []
+
   def initialize
+    @in_cart = []
+    @total_in_cart = 0
+  end
+
+  def in_cart
+    @in_cart
+  end
+
+  def total_in_cart
+    @total_in_cart
+  end
+
+  def add_product(name, base_price, tax_rate)
+    new_product = Product.new(name, base_price, tax_rate)
+    @in_cart << new_product
+    @total_in_cart += 1
+  end
+
+  def remove_product(item)
+    @in_cart.delete(item)
+  end
+
+  def total_before_tax
+    @total_before_tax = 0
+    @in_cart.each do |price|
+      @total_before_tax += price.product_base_price
+    end
+    @total_before_tax
+  end
+
+  def total_after_tax
+    @total_after_tax = 0
+    @in_cart.each do |price|
+      @total_after_tax += price.product_base_price * price.product_tax_rate
+    end
+    @total_after_tax
+  end
+
+end
+
+cart1 = Cart.new
+hair_spray = cart1.add_product("hair spray", 5, 0.013)
+p cart1.inspect
+shampoo = cart1.add_product("shampoo", 10, 0.013)
+conditioner = cart1.add_product("conditioner", 8, 0.013)
+mousse = cart1.add_product("mousse", 4, 0.013)
+p cart1.total_in_cart
+p cart1.total_before_tax
+p cart1.total_after_tax
+p cart1.inspect
+p cart1.remove_product(mousse)
+p cart1.total_in_cart
